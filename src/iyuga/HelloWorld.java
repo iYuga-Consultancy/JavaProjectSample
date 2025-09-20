10f8f1f8777ac68fd14ae73f333f90b8eeb674c3
Dell <Dell@DESKTOP-F1EKMEF.lan>
2025-09-20 12:40:50 +0530

    Updated

--- src/iyuga/HelloWorld.java
+++ src/iyuga/HelloWorld.java
@@ -3,5 +3,6 @@ package iyuga;
 public class HelloWorld {
 	public static void main(String[] args) {
         System.out.println("Hello, GitHub from Java!");
+        System.out.println("Hello, welcome to git!");
     }
 }
